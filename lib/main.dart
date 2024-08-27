bb30cc9a6ca806ca0fbcfa37b3aa8a0ad7b6bced
EmmanuelG447 <emmanuelgutierrez19878@gmail.com>
2024-08-27 13:10:56 -0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'liquid_progress_indicator/liquid_progress_indicator.dart';
 import 'mqtt_service.dart';
 
@@ -7,38 +8,83 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MQTT Humidity Level App',
+      title: 'MQTT Sensor Dashboard',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HumidityLevelScreen(),
+      home: const SensorDashboardScreen(),
+      debugShowCheckedModeBanner: false, // Quitamos la cinta de debug
     );
   }
 }
 
-class HumidityLevelScreen extends StatefulWidget {
-  const HumidityLevelScreen({super.key});
+class SensorDashboardScreen extends StatefulWidget {
+  const SensorDashboardScreen({Key? key}) : super(key: key);
 
   @override
-  HumidityLevelScreenState createState() => HumidityLevelScreenState();
+  _SensorDashboardScreenState createState() => _SensorDashboardScreenState();
 }
 
-class HumidityLevelScreenState extends State<HumidityLevelScreen> {
+class _SensorDashboardScreenState extends State<SensorDashboardScreen> {
   late MqttService _mqttService;
-  double _humidityLevel = 0.0;
+  final List<SensorData> _mq135Data = [];
+  final List<SensorData> _mq2Data = [];
+  final List<SensorData> _mq7Data = [];
+  ChartSeriesController? _mq135Controller;
+  ChartSeriesController? _mq2Controller;
+  ChartSeriesController? _mq7Controller;
+
+  double _mq135Value = 0.0;
+  double _mq2Value = 0.0;
+  double _mq7Value = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _mqttService = MqttService('broker.emqx.io', '');
-    _mqttService.getHumidityLevelStream().listen((humidityLevel) {
+    _mqttService = MqttService('10.14.55.68', 'flutter_client');
+
+    _mqttService.getMq135Stream().listen((value) {
       setState(() {
-        _humidityLevel = humidityLevel;
+        _mq135Value = value;
+        _mq135Data.add(SensorData(DateTime.now(), value));
+        if (_mq135Data.length > 20) {
+          _mq135Data.removeAt(0);
+        }
+        if (_mq135Controller != null) {
+          _mq135Controller!
+              .updateDataSource(addedDataIndex: _mq135Data.length - 1);
+        }
+      });
+    });
+
+    _mqttService.getMq2Stream().listen((value) {
+      setState(() {
+        _mq2Value = value;
+        _mq2Data.add(SensorData(DateTime.now(), value));
+        if (_mq2Data.length > 20) {
+          _mq2Data.removeAt(0);
+        }
+        if (_mq2Controller != null) {
+          _mq2Controller!.updateDataSource(addedDataIndex: _mq2Data.length - 1);
+        }
+      });
+    });
+
+    _mqttService.getMq7Stream().listen((value) {
+      setState(() {
+        _mq7Value = value;
+        _mq7Data.add(SensorData(DateTime.now(), value));
+        if (_mq7Data.length > 20) {
+          _mq7Data.removeAt(0);
+        }
+        if (_mq7Controller != null) {
+          _mq7Controller!.updateDataSource(addedDataIndex: _mq7Data.length - 1);
+        }
       });
     });
   }
@@ -47,32 +93,78 @@ class HumidityLevelScreenState extends State<HumidityLevelScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Humidity Level Gauge'),
+        title: const Text('Sensor Dashboard'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: AspectRatio(
-            aspectRatio: 1.0,
-            child: LiquidCircularProgressIndicator(
-              value: _humidityLevel / 100,
-              valueColor: const AlwaysStoppedAnimation(Colors.blue),
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              borderColor: Colors.blue,
-              borderWidth: 5.0,
-              direction: Axis.vertical,
-              center: Text(
-                '${_humidityLevel.toStringAsFixed(1)}%',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 3,
+          childAspectRatio: 1.0,
+          children: [
+            buildGauge('MQ-135', _mq135Value),
+            buildGauge('MQ-2', _mq2Value),
+            buildGauge('MQ-7', _mq7Value),
+            buildLineChart('MQ-135 Line Chart', _mq135Data, _mq135Controller),
+            buildLineChart('MQ-2 Line Chart', _mq2Data, _mq2Controller),
+            buildLineChart('MQ-7 Line Chart', _mq7Data, _mq7Controller),
+          ],
         ),
       ),
     );
   }
+
+  Widget buildGauge(String label, double value) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: LiquidCircularProgressIndicator(
+        value:
+            (value / 10000).clamp(0.0, 1.0), // Escalado y limitado entre 0 y 1
+        valueColor: const AlwaysStoppedAnimation(Colors.blue),
+        backgroundColor: Colors.transparent,
+        borderColor: Colors.blue,
+        borderWidth: 5.0,
+        direction: Axis.vertical,
+        center: Text(
+          '$label\n${value.toStringAsFixed(1)}',
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Widget buildLineChart(
+      String title, List<SensorData> data, ChartSeriesController? controller) {
+    return SfCartesianChart(
+      title: ChartTitle(text: title),
+      primaryXAxis: DateTimeAxis(),
+      primaryYAxis: NumericAxis(
+        minimum: 300,
+        maximum: 10000,
+        interval: 1000, // Ajusta según el rango de tus datos
+      ),
+      series: <LineSeries<SensorData, DateTime>>[
+        LineSeries<SensorData, DateTime>(
+          onRendererCreated: (ChartSeriesController chartController) {
+            controller = chartController;
+          },
+          dataSource: data,
+          xValueMapper: (SensorData data, _) => data.time,
+          yValueMapper: (SensorData data, _) => data.value,
+          color: Colors.blue,
+        )
+      ],
+      backgroundColor: Colors.transparent, // Sin fondo gris
+    );
+  }
+}
+
+class SensorData {
+  SensorData(this.time, this.value);
+  final DateTime time;
+  final double value;
 }
--- lib/mqtt_service.dart
+++ lib/mqtt_service.dart
@@ -5,39 +5,71 @@ class MqttService {
   final MqttServerClient client;
 
   MqttService(String server, String clientId)
-      : client = MqttServerClient(server, '') {
-    const sanitizedClientId = '';
-
+      : client = MqttServerClient(server, clientId) {
     client.logging(on: true);
     client.setProtocolV311();
     client.keepAlivePeriod = 20;
 
     final connMessage = MqttConnectMessage()
-        .withClientIdentifier(sanitizedClientId)
+        .withClientIdentifier(clientId) // Utiliza el clientId proporcionado
         .startClean()
-        .withWillQos(MqttQos.atLeastOnce);
+        .withWillQos(MqttQos.exactlyOnce); // Cambiado a exactlyOnce para QoS 2
 
     client.connectionMessage = connMessage;
   }
 
-  Stream<double> getHumidityLevelStream() async* {
-    try {
-      await client.connect();
-    } catch (e) {
-      client.disconnect();
-      return;
-    }
-
-    if (client.connectionStatus?.state == MqttConnectionState.connected) {
-      client.subscribe("humidity/level", MqttQos.atLeastOnce);
-
-      await for (final c in client.updates!) {
+  Stream<double> getMq135Stream() async* {
+    await _connectAndSubscribe('sensor/mq135');
+    await for (final c in client.updates!) {
+      if (c.isNotEmpty) {
         final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
         final String pt =
             MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
         yield double.tryParse(pt) ?? 0.0;
       }
+    }
+  }
+
+  Stream<double> getMq2Stream() async* {
+    await _connectAndSubscribe('sensor/mq2');
+    await for (final c in client.updates!) {
+      if (c.isNotEmpty) {
+        final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+        final String pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        yield double.tryParse(pt) ?? 0.0;
+      }
+    }
+  }
+
+  Stream<double> getMq7Stream() async* {
+    await _connectAndSubscribe('sensor/mq7');
+    await for (final c in client.updates!) {
+      if (c.isNotEmpty) {
+        final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+        final String pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        yield double.tryParse(pt) ?? 0.0;
+      }
+    }
+  }
+
+  Future<void> _connectAndSubscribe(String topic) async {
+    try {
+      await client.connect();
+    } catch (e) {
+      print('Error al conectar: $e');
+      client.disconnect();
+      return;
+    }
+
+    if (client.connectionStatus?.state == MqttConnectionState.connected) {
+      print('Conexión establecida con el broker MQTT.');
+      client.subscribe(
+          topic, MqttQos.exactlyOnce); // Cambiado a exactlyOnce para QoS 2
     } else {
+      print(
+          'No se pudo establecer la conexión con el broker MQTT. Estado: ${client.connectionStatus?.state}');
       client.disconnect();
     }
   }
